bcbc55d64564db1a6687a5fcf15fdae8ccd0c145
Abhishek Garg <121926531+abhishekgargbyj@users.noreply.github.com>
2023-01-12 14:59:22 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+function App() {
+  const link="http://www.google.com";
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      
+      <div className="content">
+      <Home/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>React Introduction</h1>
+        </nav>
+    );
+}
+ 
+export default Navbar;
