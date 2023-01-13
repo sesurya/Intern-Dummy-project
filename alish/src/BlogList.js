5df1b82082e32cb159662dc8456d9c7ffd9b9501
Alish Sapkota <alish.sapkota@byjus.com>
2023-01-13 16:13:30 +0530

    learned to fecth data using fake api endpoint

--- alish/src/BlogList.js
+++ alish/src/BlogList.js
@@ -1,11 +1,12 @@
-const BlogList = (props) => {
-    const blogs = props.Blogs
+const BlogList = ({blogs,title, handleDelete}) => {
     return (  
         <div className="blog-list">
+            <h2>{title}</h2>
              {blogs.map((blog)=> (
                 <div className="blog-preview" key = {blog.id}>
                     <h2> {blog.title} </h2>
                     <p> Written by {blog.author}. </p>
+                    <button className = "delete-button" onClick={()=> handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
         </div>
--- alish/src/Home.js
+++ alish/src/Home.js
@@ -1,15 +1,25 @@
 
-import { useState  } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'My new Websites' , body: 'body lorem ipsum...', author: 'mario', id:1},
-        {title: 'Welcome Party'   , body: 'body lorem ipsum...', author: 'yoshi', id:2},
-        {title: 'Web dev top tips', body: 'body lorem ipsum...', author: 'mario', id:3}
-    ]);
+    const [blogs, setBlogs] = useState(null);
+   
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter( blog => blog.id!=id)
+        setBlogs(newBlogs);
+    }
+    useEffect(()=>{
+       fetch('http://localhost:8000/blogs')
+       .then(res => {
+        return res.json();
+       })
+       .then(data => {
+            setBlogs(data);
+       })
+    },[]);
     return (  
         <div className="home">
-           <BlogList Blogs = {blogs} />
+           {blogs && <BlogList blogs = {blogs} title = "All Blogs!" handleDelete={handleDelete} />}
         </div>
     );
 }
