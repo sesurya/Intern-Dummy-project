040623d97f40a1b2cce4357eaaa8da46ead4a703
Alish Sapkota <alish.sapkota@byjus.com>
2023-01-13 11:32:08 +0530

    changed css and learned about outptting list

--- alish/src/Home.js
+++ alish/src/Home.js
@@ -1,17 +1,21 @@
 
 import { useState  } from "react";
 const Home = () => {
-    const [name, setName] = useState('hello');
-    const handleClick = ()=>{
-        name === "alish"?setName("hello"):setName("alish")
-    }
+    const [blogs, setBlogs] = useState([
+        {title: 'My new Websites' , body: 'body lorem ipsum...', author: 'mario', id:1},
+        {title: 'Welcome Party'   , body: 'body lorem ipsum...', author: 'yoshi', id:2},
+        {title: 'Web dev top tips', body: 'body lorem ipsum...', author: 'mario', id:3}
+    ]);
     return (  
         <div className="home">
-            <h2>Home Page</h2>
-            <div> {name} </div>
-            <button  onClick = {handleClick } >Click me</button>
+            {blogs.map((blog)=> (
+                <div className="blog-preview" key = {blog.id}>
+                    <h2> {blog.title} </h2>
+                    <p> Written by {blog.author}. </p>
+                </div>
+            ))}
         </div>
     );
 }
  
-export default Home;``
+export default Home;
